54c36fdad9ae603d4b4b03e9dab6f019c4ff76b7
HUAN WANG <wanghuan_hh@126.com>
2026-03-25 16:59:41 +0800

    update site title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,15 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "王欢的主页",
     pageTitleSuffix: "",
-    enableSPA: true,
+    enableSPA: false,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "lepingkyxk.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
